73d3cf434aaf80c156d8123c2c0229c68a4c4368
bhavani125 <girinenibhavani12@gmil.com>
2021-06-23 16:20:26 +0530

    Added: code for creating a contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,29 @@
 package com.book;
-
-public class AddressBook {
+ import java.util.Scanner;
+  public class AddressBook {
+    //main method
     public static void main(String[] args) {
-        System.out.println("Welcome Address Book System Program");
+
+        Scanner sc = new Scanner(System.in);
+        // creating constructor
+        Contacts book =new Contacts();
+
+        System.out.println("Enter the firstName");
+        book.setFirstName(sc.next());
+        System.out.println("Enter the lastName");
+        book.setLastName(sc.next());
+        System.out.println("Enter the address");
+        book.setAddress(sc.next());
+        System.out.println("Enter the city");
+        book.setCity(sc.next());
+        System.out.println("Enter the state");
+        book.setState(sc.next());
+        System.out.println("Enter the emailId");
+        book.setEmail(sc.next());
+        System.out.println("Enter the phoneNumber");
+        book.setPhoneNumber(sc.nextLong());
+        System.out.println("Enter the zip");
+        book.setZip(sc.nextLong());
     }
 }
+
